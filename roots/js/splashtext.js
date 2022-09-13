90d4f805f1caabb7e903f680b530d8cc3b4af617
cupofdirtfordinner <cupofdirtfordinner@gmail.com>
2022-09-13 07:24:35 -0600

    fixed css marquee + new lines
    lines are from cruelty squad, everhood, and celeste. shoutout to stackoverflow for preventing my stress induced early onset male pattern baldness

--- roots/js/splashtext.js
+++ roots/js/splashtext.js
@@ -1,8 +1,10 @@
 setInterval(10)
-fetch("roots/js/json/lines.json")
+fetch("/roots/js/json/lines.json")
 .then(res => res.json())
 .then(json => {
-    document.getElementById("splash").innerHTML = json[Math.floor(Math.random() * json.length)];
+    const splash = document.getElementById("splash")
+    splash.innerHTML = json[Math.floor(Math.random() * json.length)];
+    splash.style.animationDuration = `${Math.round(splash.clientWidth / 200)}s`
 }).catch(error => {
     console.log(error);
 });
